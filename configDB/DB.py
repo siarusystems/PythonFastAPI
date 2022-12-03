49fb657d0083bf2f12075a3ad0ced6435022acc4
siarusystems <80916431+siarusystems@users.noreply.github.com>
2022-12-03 23:46:57 +0000

    se agrega config db

---
+++ configDB/DB.py
@@ -0,0 +1,7 @@
+import mysql.connector
+
+
+cnx = mysql.connector.connect(user='monty', password='skate',
+                              host='192.168.0.250',
+                              database='hunty')
+                              
